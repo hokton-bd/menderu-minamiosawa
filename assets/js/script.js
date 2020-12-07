809493fd694f31f614e9ab15a490686f047ab28f
hokton-bd <skmhkt.p@gmail.com>
2020-12-07 19:11:30 +0900

    drawer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
 
-    $('.nav-menu__link').click(function(){
+    $('.nav-menu__link, .drawer-nav__link').click(function(){
 
             if($(this).attr('href') === '#top') {
                 var adjust = -300;
 
             } else {
                 var adjust = -150;
+            }
+
+            if($('.drawer').hasClass('drawer--active')) {
+
+                $('.drawer').removeClass('drawer--active');
+
             }
 
             var speed = 400;
@@ -24,13 +30,27 @@ $(document).ready(function() {
 
             $('.js-logo').attr('src', 'assets/images/logo-minamiosawa-w.svg');
             $('.global-nav').addClass('js-global-nav--scroll');
+            $('.drawer').addClass('js-drawer--scroll');
 
         } else if($(this).scrollTop() === 0) {
 
             $('.js-logo').attr('src', 'assets/images/logo-minamiosawa-b.svg');
             $('.global-nav').removeClass('js-global-nav--scroll');
+            $('.drawer').removeClass('js-drawer--scroll');
 
         }
+
+    });
+
+    $('.drawer__toggler').on('click', function() {
+
+        $('.drawer').addClass('drawer--active');
+
+    });
+
+    $('.drawer__closer').on('click', function() {
+
+        $('.drawer').removeClass('drawer--active');
 
     });
 
